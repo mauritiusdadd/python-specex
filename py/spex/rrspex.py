Corrected aperture computation

py/spex/rrspex.py
@@ -59,7 +59,7 @@ from redrock.archetypes import All_archetypes
 from .utils import plot_zfit_check
 
 
-def get_templates(template_types=[], filepath=False):
+def get_templates(template_types=[], filepath=False, templates=None):
     """
     Get avilable templates.
 
@@ -71,6 +71,10 @@ def get_templates(template_types=[], filepath=False):
         The default is [].
     filepath : boot, optional
         If it's true then return the file paths instead of actual templates.
+    templates : str, optional
+        The path of a template file or of a directory containing templates
+        files. If None, templates are searched in the default redrock path.
+        The default value is None.
 
     Returns
     -------
@@ -78,8 +82,11 @@ def get_templates(template_types=[], filepath=False):
         The available templates or the corresponding file paths.
 
     """
+    if templates is not None and os.path.isfile(templates):
+        return [Template(templates), ]
+
     available_templates = []
-    for t in find_templates():
+    for t in find_templates(templates):
         templ = Template(t)
         if not template_types or templ.template_type in template_types:
             if filepath:
@@ -216,7 +223,6 @@ def read_spectra(spectra_fits_list, spec_hdu=None, var_hdu=None, wd_hdu=None):
         else:
             flux = hdul[spec_hdu].data
             spec_wcs = wcs.WCS(hdul[spec_hdu].header)
-        flux = flux.astype('float32')
 
         if var_hdu is None:
             for hdu in hdul:
@@ -254,6 +260,14 @@ def read_spectra(spectra_fits_list, spec_hdu=None, var_hdu=None, wd_hdu=None):
         # NOTE: Wavelenghts must be in Angstrom units
         pixel = np.arange(len(flux))
         lam = spec_wcs.pixel_to_world(pixel).Angstrom
+
+        flux = flux.astype('float32')
+        flux_not_nan_mask = ~np.isnan(flux)
+        flux = flux[flux_not_nan_mask]
+        ivar = ivar[flux_not_nan_mask]
+        lam = lam[flux_not_nan_mask]
+        if wd is not None:
+            wd = wd[flux_not_nan_mask]
 
         # If now wavelenght dispersion information is present, then fallback
         # to a uniform resolution model (i.e. an identiry matrix)
@@ -513,7 +527,9 @@ def rrspex(options=None, comm=None):
 
         if args.plot_zfit:
             if comm_rank == 0:
-                available_templates = get_templates()
+                available_templates = get_templates(
+                    templates_dir=args.templates
+                )
                 for target in targets:
                     fig, ax = plot_zfit_check(
                         target,

py/spex/spex.py
@@ -52,7 +52,7 @@ from .utils import plot_zfit_check, getcutout, getlogimg, gethdu, getpbar
 from .utils import stack
 
 try:
-    from .rrspex import rrspex
+    from .rrspex import rrspex, get_templates
 except ImportError:
     HAS_RR = False
 else:
@@ -162,15 +162,6 @@ def __argshandler():
     parser.add_argument(
         '--invert-mask', action='store_true', default=False,
         help='Set whether to use the inverse of the cube mask.'
-    )
-
-    parser.add_argument(
-        '--no-nans', action='store_true', default=False,
-        help='Set whether to remove NaNs from the spectrum. If this argument '
-        'is specified, then the extracted spectrum is filtered and NaNs are '
-        'set to zero. If variance extension is '
-        'present, the variances corresponding to NaNs are set to very high '
-        'values.'
     )
 
     parser.add_argument(
@@ -308,6 +299,11 @@ def __argshandler():
     parser.add_argument(
         "-t", "--templates", type=str, default=None, required=False,
         help="template file or directory. " + rr_help_note
+    )
+
+    parser.add_argument(
+        "--debug", action='store_true', default=False,
+        help="Enable some debug otupout on plots and on the console."
     )
 
     args = parser.parse_args()
@@ -753,6 +749,7 @@ def spex():
         key_id = args.key_id
 
     out_specfiles = []
+    spex_apertures = {}  # these values are in physical units (arcsecs)
     for i, source in enumerate(sources[:n_objects]):
         progress = (i + 1) / n_objects
         sys.stderr.write(f"\r{getpbar(progress)} {progress:.2%}\r")
@@ -773,15 +770,32 @@ def spex():
             y_over_b /= source[args.key_b]
             y_over_b = y_over_b ** 2
 
+            spex_apertures[source[key_id]] = (
+                source[args.key_a] * pixelscale,
+                source[args.key_b] * pixelscale,
+                source[args.key_theta]
+            )
+
             mask = (x_over_a + y_over_b) < (1.0/source[args.key_kron])
 
         elif mode == 'kron_circular':
             kron_circular = source[args.key_kron] * source[args.key_b]
             kron_circular /= source[args.key_a]
 
+            spex_apertures[source[key_id]] = (
+                kron_circular * pixelscale,
+                kron_circular * pixelscale,
+                0
+            )
+
             mask = (xx_tr**2 + yy_tr**2) < (kron_circular ** 2)
 
         elif mode == 'circular_aperture':
+            spex_apertures[source[key_id]] = (
+                args.aperture_size,
+                args.aperture_size,
+                0
+            )
             mask = (xx_tr**2 + yy_tr**2) < args.aperture_size / pixelscale
 
         if cube_footprint is not None:
@@ -811,10 +825,6 @@ def spex():
         if sn_spec < args.sn_threshold:
             continue
 
-        if args.no_nans is True:
-            obj_spec_nan_mask = np.isnan(obj_spectrum)
-            obj_spectrum[obj_spec_nan_mask] = 0
-
         my_time = Time.now()
         my_time.format = 'isot'
 
@@ -841,10 +851,6 @@ def spex():
         if var_hdu is not None:
             obj_spectrum_var = trasposed_var[mask].sum(axis=0)
             sn_var = obj_mean_spec / np.sqrt(np.nanmean(obj_spectrum_var))
-
-            if args.no_nans:
-                max_var = np.nanmax(obj_spectrum_var)
-                obj_spectrum_var[obj_spec_nan_mask] = 3 * max_var
 
             main_header['SN_VAR'] = (sn_var, "")
         else:
@@ -901,7 +907,13 @@ def spex():
             fig = plt.figure(figsize=(10, 10))
             ax = plt.subplot(projection=celestial_wcs)
             logimg, cvmin, cvmax = getlogimg(stacked_cube)
-            ax.imshow(logimg, origin='lower', vmin=cvmin, vmax=cvmax)
+            ax.imshow(
+                logimg,
+                origin='lower',
+                vmin=cvmin,
+                vmax=cvmax,
+                cmap='jet'
+            )
             fig.savefig("stacked_cube.png")
             plt.tight_layout()
             plt.close(fig)
@@ -934,7 +946,13 @@ def spex():
             ]
         cut_pixelscale = np.nanmean(cut_pixelscale)
 
-        for target in targets:
+        print("", file=sys.stderr)
+        for i, target in enumerate(targets):
+            progress = (i + 1) / len(targets)
+            sys.stderr.write(
+                f"\rGenerating previews {getpbar(progress)} {progress:.2%}\r"
+            )
+            sys.stderr.flush()
             obj = sources[sources[args.key_id] == target.id][0]
 
             obj_skycoord = SkyCoord(
@@ -959,32 +977,47 @@ def spex():
                 vmax=cut_vmax
             )
 
+            if args.debug:
+                plot_templates = get_templates(args.templates)
+            else:
+                plot_templates = None
+
             fig, axs = plot_zfit_check(
                 target,
                 zfit,
-                plot_template=None,
+                plot_template=plot_templates,
                 cutout=cutout,
                 wave_units=spec_hdu.header['CUNIT3'],
                 flux_units=spec_hdu.header['BUNIT'],
             )
 
+            e_wid = spex_apertures[target.id][0] / cut_pixelscale
+            e_hei = spex_apertures[target.id][1] / cut_pixelscale
+            e_ang = spex_apertures[target.id][2]
+
             aperture = Ellipse(
                 (cutout_size/2.0, cutout_size/2.0),
-                width=args.aperture_size / cut_pixelscale,
-                height=args.aperture_size / cut_pixelscale,
+                width=e_wid,
+                height=e_hei,
+                angle=e_ang,
                 edgecolor='#0000ff',
                 facecolor='none',
                 ls='-',
+                lw=1,
+                alpha=0.8,
                 fill=False
             )
             axs[1].add_patch(aperture)
             aperture = Ellipse(
                 (cutout_size/2.0, cutout_size/2.0),
-                width=args.aperture_size / cut_pixelscale,
-                height=args.aperture_size / cut_pixelscale,
+                width=e_wid,
+                height=e_hei,
+                angle=e_ang,
                 edgecolor='#00ff00',
                 facecolor='none',
                 ls='--',
+                lw=1,
+                alpha=0.8,
                 fill=False
             )
             axs[1].add_patch(aperture)

py/spex/utils.py
@@ -36,9 +36,8 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
+from scipy.signal import savgol_filter
 from astropy.nddata import Cutout2D
-from astropy.wcs import WCS
-from astropy.io import fits
 
 from .lines import getlines
 
@@ -78,8 +77,6 @@ def getpbar(partial, total=None, wid=32, common_char='\u2588',
     pbar_full += upper_char*(total_prog - common_prog)
     pbar_full += lower_char*(prog - common_prog)
     return (f"\u2595{{:<{wid}}}\u258F").format(pbar_full)
-
-
 
 
 def gethdu(hdl, valid_names, hdu_index=-1, msg_err_notfound=None,
@@ -384,6 +381,14 @@ def plot_zfit_check(target, zbest, plot_template=None, rest_frame=True,
             lw=2,
             alpha=1,
             label='spectrum'
+        )
+
+        ax0.plot(
+            wavelenghts, savgol_filter(target_spec.flux, 51, 11),
+            ls='-',
+            lw=1,
+            alpha=0.8,
+            label='smoothed spectrum'
         )
 
         if plot_template and best_template: