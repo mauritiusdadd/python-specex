Fix loading for spectra with no variance

py/spex/rrspex.py
@@ -220,10 +220,11 @@ def read_spectra(spectra_fits_list, spec_hdu=None, var_hdu=None, wd_hdu=None):
                     ivar = hdu.data
                     break
             else:
-                raise ValueError(
-                    "Cannot determine the HDU containing variance data: "
-                    f"'{fits_file}'"
+                print(
+                    "WARNING: Cannot determine the HDU containing variance "
+                    f"data in '{fits_file}'! Using dumb constan variance...",
                 )
+                ivar = np.ones_like(flux)
         else:
             ivar = 1 / hdul[var_hdu].data
 
@@ -288,6 +289,7 @@ def read_spectra(spectra_fits_list, spec_hdu=None, var_hdu=None, wd_hdu=None):
         target = Target(target_id, [rrspec])
         target.input_file = fits_file
         target.lam_mask = lam_mask
+        target.spec_id = spec_id
         targets.append(target)
         targetids.append(target_id)
         specids.append(spec_id)

py/spex/spex.py
@@ -444,7 +444,8 @@ def get_spsingle_fits(main_header, spec_wcs_header, obj_spectrum,
     if no_nans:
         nanmask = np.isnan(obj_spectrum)
         obj_spectrum[nanmask] = 0
-        obj_spectrum_var[nanmask] = np.nanmax(obj_spectrum_var) * 100.0
+        if obj_spectrum_var is not None:
+            obj_spectrum_var[nanmask] = np.nanmax(obj_spectrum_var) * 100.0
     else:
         nanmask = None
 
@@ -794,7 +795,7 @@ def spex():
         sys.stderr.flush()
 
         if key_id is not None:
-            obj_id = f"{str(source[key_id]):0>6}"
+            obj_id = source[key_id]
         else:
             obj_id = f"{i:06}"
 
@@ -1025,7 +1026,7 @@ def spex():
                 f"\rGenerating previews {get_pbar(progress)} {progress:.2%}\r"
             )
             sys.stderr.flush()
-            obj = sources[source_ids == target.id][0]
+            obj = sources[source_ids == target.spec_id][0]
 
             obj_skycoord = SkyCoord(
                 obj[args.key_ra],
@@ -1058,9 +1059,9 @@ def spex():
                 flux_units=spec_hdu.header['BUNIT'],
             )
 
-            e_wid = spex_apertures[target.id][0] / cut_pixelscale
-            e_hei = spex_apertures[target.id][1] / cut_pixelscale
-            e_ang = spex_apertures[target.id][2]
+            e_wid = spex_apertures[target.spec_id][0] / cut_pixelscale
+            e_hei = spex_apertures[target.spec_id][1] / cut_pixelscale
+            e_ang = spex_apertures[target.spec_id][2]
 
             aperture = Ellipse(
                 (cutout_size/2.0, cutout_size/2.0),
@@ -1110,13 +1111,13 @@ def spex():
                 }
             )
 
-            figname = f'spectrum_{target.id}.png'
+            figname = f'spectrum_{target.spec_id}.png'
             figname = os.path.join(check_images_outdir, figname)
             fig.savefig(figname, dpi=150)
             plt.close(fig)
 
             if args.debug:
-                figname = f'scandata_{target.id}.png'
+                figname = f'scandata_{target.spec_id}.png'
                 figname = os.path.join(check_images_outdir, figname)
                 fig, axs = plot_scandata(target, scandata)
                 fig.savefig(figname, dpi=150)

test/data/make_synt_cube.py
@@ -7,9 +7,240 @@ Copyright (C) 2022  Maurizio D'Addona <mauritiusdadd@gmail.com>
 """
 import numpy as np
 from astropy.io import fits
+from astropy.modeling import models
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+from redrock.templates import find_templates, Template
+from matplotlib import patches
+from astropy.table import Table
+from astropy.wcs import WCS
+
+from spex import utils
 
 
-def gen_fake_spectrum(wave_range, template, coeffs, z, wave_step=1):
+class FakeObject():
+    """
+    Class to handle fake object generation.
+
+    This class contains morphological properties of an object, such as the
+    the shape of the isophotal ellipse at kron radius, as well as physical
+    properties as its spectral tepmlate and luminosity.
+    """
+
+    __template_dict = {}
+
+    def __init__(self, template_file, x_image, y_image, z=None, coeffs=None,
+                 surface_bightness_profile=None):
+        """
+        Initialize the class.
+
+        Parameters
+        ----------
+        template : redrock.template.Template
+            The spectral template of the object.
+        x_image : float
+            The x position of the center of the object in the image.
+        y_image : float
+            The y position of the center of the object in the image.
+        coeffs : list or numpy.ndarray
+            Coefficients uesd for generating the spectrum, passed to the method
+            template.eval().
+        z : float
+            Redshift of the spectrum.
+        surface_bightness_profile : astropy.modeling.Fittable2DModel, optional
+            The surface brightness profile model. If None, a Sersic2D model is
+            used with random ellipticity and theta. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.x_image = x_image
+        self.y_image = y_image
+        self.z = z
+
+        if template_file not in self.__template_dict:
+            self.__template_dict[template_file] = Template(template_file)
+
+        self.template_file = template_file
+
+        f = fits.open(template_file)
+        try:
+            has_archetypes = f[1].name == 'ARCHETYPE_COEFF'
+        except IndexError:
+            has_archetypes = False
+
+        if has_archetypes:
+            archetype_coeffs = f[1].data
+            coeff_indexes = len(archetype_coeffs)
+            self.__coeffs = archetype_coeffs[np.random.choice(coeff_indexes)]
+        else:
+            coeff_indexes = range(self.template.nbasis)
+            self.__coeffs = np.zeros_like(coeff_indexes)
+            self.__coeffs[np.random.choice(coeff_indexes)] = 1
+
+        if surface_bightness_profile is None:
+            obj_type = self.template.template_type
+            obj_type = obj_type.lower()
+            if obj_type == 'star':
+                n_val = 99
+                amp_val = 1
+                e_val = 0
+                r_eff_val = 2
+                if self.z is None:
+                    self.z = 0
+            elif obj_type == 'galaxy':
+                n_val = 1 + np.random.random()*5
+                amp_val = 10 + np.random.random()*5
+                e_val = np.random.random()*0.6
+                r_eff_val = np.random.normal(loc=2)**2
+                if self.z is None:
+                    self.z = 0
+                    while self.z < 0.2:
+                        self.z = np.random.choice(self.template.redshifts)
+            else:
+                n_val = 99
+                amp_val = 5.0e-6
+                e_val = 0
+                r_eff_val = 1.5
+                if self.z is None:
+                    self.z = 0
+                    while self.z < 1:
+                        self.z = np.random.choice(self.template.redshifts)
+
+            self.surface_bightness_profile_function = models.Sersic2D(
+                amplitude=amp_val,
+                r_eff=r_eff_val,
+                n=n_val,
+                x_0=self.x_image,
+                y_0=self.y_image,
+                ellip=e_val,
+                theta=np.random.random() * 2 * np.pi
+            )
+        else:
+            self.surface_bightness_profile_function = surface_bightness_profile
+
+    @property
+    def template(self):
+        """
+        Get the spectral template of the object.
+
+        Returns
+        -------
+        template : redrock.template.Template
+            The spectral template of the object.
+
+        """
+        return self.__template_dict[self.template_file]
+
+    def get_image(self, width, height, seeing=1):
+        """
+        Get the an image containing the object.
+
+        Parameters
+        ----------
+        width : int
+            The image width.
+        height : int
+            The image height.
+        seeing: float
+            Parameter of a gaussian blur filter.
+
+        Returns
+        -------
+        img : 2D numpy.ndarray
+            The image containing the object.
+
+        """
+        x, y = np.meshgrid(np.arange(width), np.arange(height))
+        base_image = self.surface_bightness_profile_function(x, y)
+
+        base_image += gaussian_filter(base_image, seeing)
+        return base_image
+
+    def get_spectrum(self, wave_range, wave_step=1):
+        """
+        Get the spectrum of the object.
+
+        Parameters
+        ----------
+        wave_range : tuple or numpy.ndarray
+            The range of wavelenght, in Angstrom, the sythetic spectrum should
+            cover. Only maximum and minimum values of this parameter are used.
+
+        Returns
+        -------
+        wave : numpy.ndarray
+            Wavelenghts, in Angstrom, used to generate spectral data
+        flux : numpy.ndarray
+            Fluxes, in arbitrary units, at the different wavelengths.
+
+        """
+        wave, flux = gen_fake_spectrum(
+            wave_range,
+            self.template,
+            coeffs=self.__coeffs,
+            z=self.z,
+            wave_step=wave_step
+        )
+        return wave, flux
+
+    def get_cube(self, width, height, wave_range,  wave_step=1, seeing=1):
+        """
+        Get a datacube containing the spectrum of the object.
+
+        Parameters
+        ----------
+        width : int
+            The image width.
+        height : int
+            The image height.
+        wave_range : tuple or numpy.ndarray
+            The range of wavelenght, in Angstrom, the sythetic spectrum should
+            cover. Only maximum and minimum values of this parameter are used.
+        wave_step : float, optional
+            Resolution of the spectrum in Angstrom. The default is 1.
+
+        Returns
+        -------
+        cube : 3D numpy.ndarray
+            The spectral datacybe of the object.
+
+        """
+        waves, flux = self.get_spectrum(wave_range, wave_step)
+        image = self.get_image(width, height, seeing)
+        cube = image[..., None] * flux
+        noise = np.random.random(size=cube.shape) * 0.01
+        cube = cube + noise
+        cube = cube.transpose(2, 0, 1)
+        return cube
+
+
+def get_waves(wave_range, wave_step=1.0):
+    """
+    Generate a wavelength grid given an input wavelenght range.
+
+    Parameters
+    ----------
+    wave_range : tuple or numpy.ndarray
+        The range of wavelenght, in Angstrom, the sythetic spectrum should
+        cover. Only maximum and minimum values of this parameter are used.
+
+    Returns
+    -------
+    wave : numpy.ndarray
+        Wavelenghts, in Angstrom.
+
+    """
+    # generate a wavelenght grid
+    w_start = np.min(wave_range)
+    w_end = np.max(wave_range)
+    wave = np.arange(w_start, w_end, step=wave_step)
+    return wave
+
+
+def gen_fake_spectrum(wave_range, template, coeffs, z, wave_step=1.0):
     """
     Generate a synthetic spectrum using a specific template.
 
@@ -36,7 +267,7 @@ def gen_fake_spectrum(wave_range, template, coeffs, z, wave_step=1):
     coeffs : list or numpy.ndarray
         Coefficients uesd for generating the spectrum, passed to template.eval
         method.
-    z : TYPE
+    z : float
         Redshift of the spectrum.
     wave_step : float, optional
         Resolution of the spectrum in Angstrom. The default is 1.
@@ -48,11 +279,184 @@ def gen_fake_spectrum(wave_range, template, coeffs, z, wave_step=1):
     flux : numpy.ndarray
         Fluxes, in arbitrary units, at the different wavelengths.
     """
-    # generate a wavelenght grid
-    w_start = np.min(wave_range)
-    w_end = np.max(wave_range)
-    wave = np.arange(w_start, w_end, step=wave_step)
-
+    wave = get_waves(wave_range, wave_step)
     flux = template.eval(coeffs, wave, z)
+    return wave, flux - np.min(flux)
 
-    return wave, flux
+
+def gen_fake_cube(width, height, wave_range, n_objects, wave_step=1.0,
+                  seeing=1):
+
+    waves = get_waves(wave_range, wave_step)
+
+    header = fits.Header()
+
+    header['BUNIT'] = '10**(-20)*erg/s/cm**2/Angstrom'
+    header['CRPIX1'] = 1.0
+    header['CRPIX2'] = 1.0
+    header['CD1_1'] = 5.0e-5
+    header['CD1_2'] = 0.0
+    header['CD2_1'] = 0.0
+    header['CD2_2'] = 5.0e-5
+    header['CUNIT1'] = 'deg     '
+    header['CUNIT2'] = 'deg     '
+    header['CTYPE1'] = 'RA---TAN'
+    header['CTYPE2'] = 'DEC--TAN'
+    header['CRVAL1'] = 90.0
+    header['CRVAL2'] = 0.0
+
+    my_wcs = WCS(header)
+
+    available_templates = [
+        f for f in find_templates()
+        if 'ez' not in f and (
+            'star-K' in f or
+            'star-M' in f or
+            'galaxy' in f or
+            'qso' in f
+        )
+    ]
+
+    obj_attributes = zip(
+        np.random.random(n_objects) * width,
+        np.random.random(n_objects) * height,
+        np.random.choice(available_templates, n_objects)
+    )
+
+    objects = [
+        FakeObject(
+            attr[2],
+            attr[0], attr[1],
+        )
+        for attr in obj_attributes
+    ]
+
+    base_cube = np.zeros((waves.shape[0], height, width))
+    for j, obj in enumerate(objects):
+        try:
+            base_cube += obj.get_cube(
+                width, height,
+                wave_range, wave_step,
+                seeing
+            )
+        except ValueError:
+            print(f"WARNING: Skipping object {j}")
+            continue
+
+    stacked_cube = utils.stack(base_cube)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.subplot()
+
+    log_image, vmin, vmax = utils.get_log_img(stacked_cube)
+
+    ax.imshow(
+        log_image,
+        origin='lower',
+        vmin=vmin,
+        vmax=vmax,
+        cmap='gray'
+    )
+
+    myt = Table(
+        names=[
+            'NUMBER',
+            'X_IMAGE',
+            'Y_IMAGE',
+            'A_IMAGE',
+            'B_IMAGE',
+            'THETA_IMAGE',
+            'KRON_RADIUS',
+            'ALPHA_J2000',
+            'DELTA_J2000',
+            'TRUE_Z',
+        ]
+    )
+    regfile_text = 'fk5\n'
+    for i, obj in enumerate(objects):
+        obj_x = int(obj.x_image)
+        obj_y = int(obj.y_image)
+        obj_kron = obj.surface_bightness_profile_function.r_eff.value
+        e_val = obj.surface_bightness_profile_function.ellip.value
+        obj_a_image = obj_kron
+        obj_b_image = obj_a_image * (1 - e_val)
+        obj_theta_image = obj.surface_bightness_profile_function.theta.value
+        obj_theta_image = np.rad2deg(obj_theta_image)
+
+        sky_coords = my_wcs.pixel_to_world(obj.x_image, obj.y_image)
+
+        obj_ra = sky_coords.ra.deg
+        obj_dec = sky_coords.dec.deg
+
+        ellipse = patches.Ellipse(
+            (obj_x, obj_y),
+            width=obj_a_image,
+            height=obj_b_image,
+            angle=obj_theta_image,
+            edgecolor='red',
+            facecolor='none',
+            ls='-',
+            lw=1,
+            alpha=0.8,
+            fill=False
+        )
+
+        ax.add_patch(ellipse)
+        regfile_text += f"ellipse({obj_ra:.6f}, {obj_dec:.6f}, "
+        regfile_text += f"{obj_a_image:.4f}, "
+        regfile_text += f"{obj_b_image:.4f}, {obj_theta_image:.4f}) "
+        regfile_text += f"# text={{S{i:06d}}}\n"
+
+        new_row = [
+            i, obj_x, obj_y, obj_a_image, obj_b_image, obj_theta_image,
+            obj_kron, obj_ra, obj_dec, obj.z
+        ]
+
+        myt.add_row(new_row)
+
+    with open("test_cube.reg", "w") as f:
+        f.write(regfile_text)
+
+    myt.write("test_cube_cat.fits", overwrite=True)
+
+    plt.show()
+
+    plt.close(fig)
+
+    header['CRVAL3'] = waves[0]
+    header['CRPIX3'] = 1.0
+    header['CTYPE3'] = 'WAVE'
+    header['CD3_3'] = wave_step
+    header['CD1_3'] = 0.0
+    header['CD2_3'] = 0.0
+    header['CD3_1'] = 0.0
+    header['CD2_1'] = 0.0
+    header['CUNIT3'] = 'Angstrom'
+    header['OBJECT'] = 'SYNTHETIC'
+    header['PCOUNT'] = 0
+    header['GCOUNT'] = 1
+
+    cube_hdu = fits.ImageHDU(
+        data=base_cube,
+        name='SPECTRUM',
+        header=header
+    )
+
+    cube_hdu.writeto('test_cube.fits', overwrite=True)
+
+
+def main():
+    """
+    Run the main program of this module.
+
+    Returns
+    -------
+    None.
+
+    """
+    wave_range = (4500, 8000)
+    gen_fake_cube(256, 256, wave_range, n_objects=30)
+
+
+if __name__ == '__main__':
+    main()